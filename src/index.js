124784271a7b63d35f84b2e9c23942eb265e5db0
juhid30 <55.juhideore@gmail.com>
2024-05-09 15:22:12 +0530

    cookie-parser and cors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import dotenv from "dotenv";
 import { connectDB } from "./db/index.js";
 
+import { app } from "./app.js";
+
 dotenv.config({
   path: "./.env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("ERR: ", error);
+      throw error;
+    }); // error with application
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port ${process.env.PORT}`);
+    });
+  })
+
+  .catch((error) => {
+    console.log("Couldn't connect database");
+  });
